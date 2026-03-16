c6d8d44f440d167599f3b4af862c86f1240b43ef
Salim <slmbouskine93@gmail.com>
2026-03-16 04:48:46 +0100

    amélioration du footer sur mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import HomeHero from "@/components/home/HomeHero";
 
 export default function Home() {
   return (
-    <div className="p-4">
+    <>
       <HomeHero />
-      <footer className="xl:hidden w-full py-4 mt-4 text-center text-cream/40 text-sm font-satoshi uppercase border border-gold/40 rounded-md">
-        © 2026 | Créé par <span className="font-bold">SAKAB</span>
-      </footer>
-    </div>
+      <div className="p-4">
+          <footer className="xl:hidden w-full py-4 text-center text-cream/40 text-sm font-satoshi uppercase border border-gold/40 rounded-md">
+            © 2026 | Créé par <span className="font-bold">SAKAB</span>
+          </footer>
+      </div>
+    </>
   );
 }
